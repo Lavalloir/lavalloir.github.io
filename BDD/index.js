58d69142f02f582348a7ab286fb31757fc480ffb
Lavalloir Clément <lavalloir.clement69@gmail.com>
2025-05-11 11:30:27 +0200

    maj compétense et project

--- BDD/index.js
+++ BDD/index.js
@@ -1,20 +1,57 @@
-// Smooth scroll for anchor links
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
+$(document).ready(function() {
+$('.slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  centerMode: true,
+  variableWidth: true,
+  infinite: true,
+  focusOnSelect: true,
+  cssEase: 'linear',
+  touchMove: true,
+  prevArrow:'<button class="slick-prev"> < </button>',
+  nextArrow:'<button class="slick-next"> > </button>',
+  
+  //         responsive: [                        
+  //             {
+  //               breakpoint: 576,
+  //               settings: {
+  //                 centerMode: false,
+  //                 variableWidth: false,
+  //               }
+  //             },
+  //         ]
+});
 
-        // Form submission handling
-        document.querySelector('form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            // Add form handling logic here
-        });
 
-        // Search functionality
-        document.querySelector('.search-button').addEventListener('click', function() {
-            // Add search handling logic here
-        });
+var imgs = $('.slider img');
+imgs.each(function(){
+  var item = $(this).closest('.item');
+  item.css({
+    'background-image': 'url(' + $(this).attr('src') + ')', 
+    'background-position': 'center',            
+    '-webkit-background-size': 'cover',
+    'background-size': 'cover', 
+  });
+  $(this).hide();
+});
+
+$('a[data-modal]').click(function(event) {
+    $(this).modal();
+    return false;
+  });
+
+  // Modal
+
+//   $('#showModal').click(() => {
+//     // $('#modalId').modal();
+//     // console.log($(this))
+//   });
+    $('.slider .item').click(function() {
+        console.log(this)
+        $('#modalId img')[0].src = $(this).find('img')[0].src;
+        $('#modalId').modal();
+    });
+  
+});
